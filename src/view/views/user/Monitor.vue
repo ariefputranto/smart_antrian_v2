96f9fc39380e12ddb58ed603c44b771fcc541f05
Arief Putranto <ariefputranto0@gmail.com>
2020-03-31 10:16:38 +0700

    fix monitor page view

--- src/view/views/user/Monitor.vue
+++ src/view/views/user/Monitor.vue
@@ -8,38 +8,63 @@
 	    <section class="content">
 	      <div class="container">
 	        <div class="row">
-	        	<div class="col-lg-12">
+	        	<div class="col-lg-4">
 	        		<div class="card">
 	              <div class="card-header">
-	                <h3 class="card-title">Loket</h3>
-	                <div class="card-tools">
-	                  <ul class="nav nav-pills ml-auto">
-	                    <li class="nav-item">
-	                      <a class="nav-link" href="#" data-toggle="modal" data-target="#search-modal"><i class="fa fa-search"></i> Search</a>
-	                    </li>
-	                    <li class="nav-item">
-	                      <a class="nav-link" href="#" data-toggle="modal" data-target="#add-modal" @click="setIsCreate(true)"><i class="fa fa-plus"></i> Add Loket</a>
-	                    </li>
-	                  </ul>
-	                </div>
+	                <h3 class="card-title"><i class="fas fa-user"></i> Loket A</h3>
 	              </div>
 	              
 	              <div class="card-body">
-			        		<table class="table table-stripped table-bordered">
-			        			<thead>
-			        				<tr>
-			        					<th>Name</th>
-			        					<th>Service</th>
-			        					<th>Token Expire</th>
-			        					<th>Latitude</th>
-			        					<th>Longitude</th>
-			        					<th>Inner Range</th>
-			        					<th>Outer Range</th>
-			        					<th>Assigned User</th>
-			        					<th>Action</th>
-			        				</tr>
-			        			</thead>
-			        		</table>
+			        		<div class="row">
+			        			<div class="col-lg-12 text-center">
+			        				<h1 style="font-weight: 400;font-size: 70px">A11</h1>
+			        			</div>
+			        		</div>
+	              </div>
+	            </div>
+	        	</div>
+	        	<div class="col-lg-4">
+	        		<div class="card">
+	              <div class="card-header">
+	                <h3 class="card-title"><i class="fas fa-user"></i> Loket A</h3>
+	              </div>
+	              
+	              <div class="card-body">
+			        		<div class="row">
+			        			<div class="col-lg-12 text-center">
+			        				<h1 style="font-weight: 400;font-size: 70px">A11</h1>
+			        			</div>
+			        		</div>
+	              </div>
+	            </div>
+	        	</div>
+	        	<div class="col-lg-4">
+	        		<div class="card">
+	              <div class="card-header">
+	                <h3 class="card-title"><i class="fas fa-user"></i> Loket A</h3>
+	              </div>
+	              
+	              <div class="card-body">
+			        		<div class="row">
+			        			<div class="col-lg-12 text-center">
+			        				<h1 style="font-weight: 400;font-size: 70px">A11</h1>
+			        			</div>
+			        		</div>
+	              </div>
+	            </div>
+	        	</div>
+	        	<div class="col-lg-4">
+	        		<div class="card">
+	              <div class="card-header">
+	                <h3 class="card-title"><i class="fas fa-user"></i> Loket A</h3>
+	              </div>
+	              
+	              <div class="card-body">
+			        		<div class="row">
+			        			<div class="col-lg-12 text-center">
+			        				<h1 style="font-weight: 400;font-size: 70px">A11</h1>
+			        			</div>
+			        		</div>
 	              </div>
 	            </div>
 	        	</div>
